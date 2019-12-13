3f0f7d64649094456ab5916073a9e76584d63a2f
Alvardud <alvaro_dudutex@outlook.es>
2019-12-13 15:07:01 -0400

    add inventary page init

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hackaton_pyme_2019/ui/pages/inventory/inventory.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark));
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+              body1: TextStyle(color: Colors.black, fontSize: 18.0,fontWeight: FontWeight.w300),
+              body2: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold,fontSize: 16.0),
+              title: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 24.0))),
+      home: Inventory(),
     );
   }
 }
---
+++ lib/ui/pages/inventory/widgets/card_view_product.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:hackaton_pyme_2019/models/insume.dart';
+
+class CardViewProduct extends StatelessWidget {
+  final Insume product;
+  CardViewProduct({this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12,
+                blurRadius: 5.0,
+                spreadRadius: 1.0,
+                offset: Offset(3.0, 3.0))
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              child: Center(
+                  child: Text(
+            product.nombre,
+            style: Theme.of(context).textTheme.body1,
+          ))),
+          Column(
+            children: <Widget>[
+              Text("${product.precio}\t\$",
+                  style: Theme.of(context).textTheme.body2),
+              RichText(
+                text: TextSpan(children: [
+                  TextSpan(text: "cantidad:\t",style: Theme.of(context).textTheme.body2),
+                  TextSpan(
+                      text: "${product.cantidad}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                          color:
+                              product.cantidad < 10 ? Colors.red : Colors.grey))
+                ]),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
